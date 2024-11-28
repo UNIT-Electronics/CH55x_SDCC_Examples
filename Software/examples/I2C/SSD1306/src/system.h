e43379c297c682e850deb51ccdd65c0df02d697b
Cesarbautista10 <devuging@gmail.com>
2024-11-28 09:39:13 -0600

    docs: update file to include license terms

--- Software/examples/I2C/SSD1306/src/system.h
+++ Software/examples/I2C/SSD1306/src/system.h
@@ -1,4 +1,13 @@
-// ===================================================================================
+
+// The original code is licensed under the Creative Commons Attribution-ShareAlike 3.0
+// Unported License (CC BY-SA 3.0). For more details, visit:
+// http://creativecommons.org/licenses/by-sa/3.0/
+//
+// Modifications and additional code in this file are licensed under the GNU General
+// Public License v3.0 (GPL-3.0). For more information, see:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// 2022 by// ===================================================================================
 // Basic System Functions for CH551, CH552 and CH554                          * v1.3 *
 // ===================================================================================
 //
